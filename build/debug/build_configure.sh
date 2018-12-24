16c1952756300d9306bc3139e76a8eaa034b2153
Mingjie Liu <jayliu@cerc.utexas.edu>
2018-12-24 15:54:13 -0600

    Binary Free Version

--- build/debug/build_configure.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-find . ! -name 'build_configure.sh' -type f | xargs rm
-find . -maxdepth 1 -type d ! -name '.' | xargs rm -r
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../../
